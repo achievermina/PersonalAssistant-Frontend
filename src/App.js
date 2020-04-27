5033c4ef52bac95ff5a179f4ca743c88d8d5df0b
achievermina <lmina820@gmail.com>
2020-04-27 12:24:41 +0900

    Seperate cookie login and new login and fix the new user login

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 import Cookies from 'js-cookie';
 import { Grid } from '@material-ui/core'
 import { SearchBar} from './components'
-import {login} from './components/UserFunction'
+import {newlogin, cookielogin} from './components/UserFunction'
 import {searchJob} from './components/IndeedClone'
 
 import GoogleLogin from "react-google-login";
 
 
 import MainTemplate from './components/MainTemplate'
+import axios from "axios";
 
 class App extends React.Component{
     constructor(props){
             super(props);
             this.state = {
-                isSignedIn: false,//localStorage.getItem('myToken') ? true : false,
+                // isSignedIn: false,//localStorage.getItem('myToken') ? true : false,
                 User: {
                     email: '',
                     id:'',
@@ -28,71 +29,132 @@ class App extends React.Component{
     componentDidMount() {
             //우리서버에 확인
             //local storage 에서 마이토큰 가져와 -> 우리서버에 넘겨서 로그인되어있는지(토큰을 받아서 JWT 유효한지 확인)
-            if (this.state.User.myToken != "undefined") {
-                const token = login(this.state.User)
-                this.loggedIn(token)
-            }
+            if (this.state.User.myToken !== undefined) {
+                const user = cookielogin(this.state.User)
+                this.loggedIn(user.myToken, user)
+            } //else {
+            //     //
+            // }
     }
 
     loggedIn = (token, newUser)  => {
          this.setState({
-            isSignedIn: true,
             User: newUser
          })
     }
 
     logout = () => {
         this.setState({
-            isSignedIn: false,
-            myToken: '',
-            user: null})
+            User: null
+        })
     };
 
-    responseGoogle = (response)  => {
-        debugger
+     responseGoogle = async (response)  =>  {
         const user = {
             email: response.profileObj.email,
             id: response.profileObj.googleId,
+            name: response.profileObj.name,
             googleToken: response.tokenObj.id_token,
+            accessToken: response.tokenObj.access_token,
+            googleExpiresAt: response.tokenObj.expires_at,
             myToken:''
         }
         console.log(user.email, user.id, user.googleToken);
         console.log(response);
 
-        const result = login(user)
-        debugger
-
-        const loggedInUser = result[0]
-        const token = result[1]
-        if (token == undefined) {
-            console.log("error im here")
-            console.log(user.email, user.id, user.myToken);
+        const result = await newlogin(user)
+         if(result[0] == false){
+             console.log("error im here");
             this.onFailure()
-        } else {
-            const newStateUser = {
-                    email:loggedInUser["email"],
-                    id:loggedInUser["id"],
-                    name:loggedInUser["name"],
-                    myToken: token
+         }else{
+             const token = result[1]
+              Cookies.set('myToken', token, {expires: 7})
+             const newStateUser = {
+                    email:user.email,
+                    id: user.id,
+                    name:user.name,
+                    myToken: token,
             }
             this.loggedIn(token, newStateUser)
-            console.log(user.email, user.id, user.myToken);
+             console.log(user.email, user.id, user.myToken);
         }
+
+
+
     }
+
+
+    //////// 질문.
+    // responseGoogle = (response)  => {
+    //     debugger
+    //     const user = {
+    //         email: response.profileObj.email,
+    //         id: response.profileObj.googleId,
+    //         googleToken: response.tokenObj.id_token,
+    //         myToken:''
+    //     }
+    //     console.log(user.email, user.id, user.googleToken);
+    //     console.log(response);
+    //
+    //     // newlogin(user)
+    //     const request = axios.post("http://127.0.0.1:5000/login", {
+    //         googleToken: user.googleToken,
+    //         email: user.email,
+    //         myToken: user.myToken
+    //     })
+    //     console.log(user.email, user.id, user.myToken);
+    //
+    //     request.then(response => {
+    //         debugger;
+    //         if (response.data.ok == false) {
+    //             throw new Error("response undefined")
+    //             this.loginUser([undefined, undefined])
+    //         }
+    //         Cookies.set('myToken', response.data.myToken, {expires: 7})
+    //         this.loginUser([response.data.user, response.data.myToken])
+    //     }).catch(error => console.error(error))
+    // }
+    //
+    // loginUser = (result) => {
+    //     debugger
+    //
+    //     const loggedInUser = result[0]
+    //     const token = result[1]
+    //     if (token == undefined) {
+    //         console.log("error im here")
+    //         this.onFailure()
+    //     } else {
+    //         const newStateUser = {
+    //                 email:loggedInUser["email"],
+    //                 id:loggedInUser["id"],
+    //                 name:loggedInUser["name"],
+    //                 myToken: token
+    //         }
+    //         this.loggedIn(token, newStateUser)
+    //         // console.log(user.email, user.id, user.myToken);
+    //     }
+    // }
+
 
     onFailure = () => {
         this.setState({
-            isSignedIn: false,
+            // isSignedIn: false,
+            User: {
+                    email: '',
+                    id:'',
+                    name:'',
+                    myToken: ''
+            }
         })
     }
 
-   handleSearchSubmit = (searchTerm) => {
+   handleSearchSubmit = async (searchTerm) => {
         console.log("handle search submit")
         // const response = await youtube.get('search',{
         //     params: {
         //         part: 'snippet',
         //         maxResults: 5,
-        //         key: '[YOUTUBE KEY]',
+        //         key: process.env.YOUTUBE_KEY,
         //         q:searchTerm
         //     }
         // });
@@ -101,13 +163,15 @@ class App extends React.Component{
         // this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
     }
 
-   handleIndeedClone = (searchTerm) => {
-        console.log("handle search submit")
-        const jobList = searchJob(searchTerm)
+   handleIndeedClone = async (searchTerm) => {
+        console.log("job Search")
+       debugger;
+        const jobList = await searchJob(searchTerm)
     }
 
     render() {
-        let content = !!this.state.isSignedIn ?
+         let content = (this.state.User.myToken !== undefined) ?
+        // let content = !!this.state.isSignedIn ?  // this.state.User.myToken !== undefined
             (
             <Grid justify = "center"  container spacing = {10}>
                 <Grid item xs = {10}>
@@ -137,6 +201,7 @@ class App extends React.Component{
                         onSuccess={this.responseGoogle}
                         onFailure={this.onFailure}
                         scope = 'https://www.googleapis.com/auth/calendar'
+                        accessType={'offline'}
                       />
                 </div>
             );
--- src/components/UserFunction.js
+++ src/components/UserFunction.js
@@ -1,27 +1,24 @@
 import axios from 'axios'
 import Cookies from 'js-cookie';
+import {loginUser} from '../App.js'
 
 
-export const login = (User) => {
-        debugger;
-
-    return axios
-        .post("http://127.0.0.1:5000/login", {
-            googleToken: User.googleToken,
-            email: User.email,
-            myToken: User.myToken
-        })
-        .then(response => {
-            Cookies.set('myToken', response.data.myToken, { expires: 7 })
-            debugger;
-            return [response.data.user, response.data.myToken]
-        })
-        .catch(err => {
-            console.log(err)
-            return ""
-        })
+export const newlogin = (User) => {
+    debugger
+    return axios.post("http://127.0.0.1:5000/login", {
+        googleToken: User.googleToken,
+        email: User.email,
+        myToken: User.myToken
+    }).then(response => [response.data.ok, response.data.token]
+    ).catch(error => console.error(error))
 }
 
+export const cookielogin = (User) => {
+    return axios.get("http://127.0.0.1:5000/cookielogin", {
+        jwtToken: User.myToken
+    }).then(response => response.data.user
+    ).catch(error => console.error(error))
+}
 
 export const calendar = (User) => {
     return axios
@@ -29,8 +26,7 @@ export const calendar = (User) => {
             googleToken: User.id,
             email: User.email,
             myToken: User.myToken,
-        })
-        .then(response => {
+        }).then(response => {
             Cookies.set('myToken', response.data.myToken, { expires: 7 })
             return response.data.myToken
         })
